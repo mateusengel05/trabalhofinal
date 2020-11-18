7ca7be5e13fec8382714502523d6fe11e03e5473
Mateus França <53907051+mateusengel05@users.noreply.github.com>
2020-11-18 12:48:40 -0300

    Update cidade.php

--- trabalho/cidade.php
+++ trabalho/cidade.php
@@ -1,20 +1,27 @@
+<head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+</head>
+
+    <form>
+  <div class="form-row">
+    <div class="col-7">
+      <input type="text" class="form-control" placeholder="Nome">
+    </div>
+    <div class="col">
+      <input type="text" class="form-control" placeholder="Estado">
+    </div>
+    <div class="col">
+      <input type="text" class="form-control" placeholder="Codigo">
+    </div>
+  </div>
+  <br>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+        
+        
+
 <?php
-
-
-if(isset($_POST['descricao']) && !empty($_POST['descricao'])) {
-    cadastrarCidade($_POST['descricao']);
-}
-
-if (isset($_GET['acao']) && (isset($_GET['registro'])) == 'deletar') {
-    deletarCidade($_GET['registro']);
-}
-
-function montaFormCidade() {
-    ?>
-    <form action ="cidade.php" method="POST"> 
-        Nome: <input type="text" name="descricao">
-        <br>
-        <button type="submit">Enviar</button>
-    </form>
-    <?php    
-}
