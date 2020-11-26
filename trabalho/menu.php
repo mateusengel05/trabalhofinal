4fee29a3a68212eb3dfe3eb99925ed138ee1bdc0
Mateus <mateus.franca@unidavi.edu.br>
2020-11-26 13:40:26 -0300

    att

--- trabalho/menu.php
+++ trabalho/menu.php
@@ -1,40 +1,21 @@
 <head>
-    <!-- CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-
-    <!-- jQuery and JS bundle w/ Popper.js -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 </head>
 
-<body>
-    <nav class="navbar navbar-dark bg-dark">
-        <a class="navbar-brand" href="#">Home</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul class="navbar-nav">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Cidades <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Estados</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Pessoas</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Saiba mais!
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                        <a class="dropdown-item" href="#">Action</a>
-                        <a class="dropdown-item" href="#">Another action</a>
-                        <a class="dropdown-item" href="#">Something else here</a>
-                    </div>
-                </li>
-            </ul>
-        </div>
-    </nav>
-</body>
+<?php
+include('conexao.php');
+$query = "SELECT * FROM menu ORDER BY ordem,descricao";
+$result = mysqli_query($conexao, $query);
+?>
+<div class="row">
+    <?php
+    foreach ($result as $linha) {
+    ?>
+        <a href="<?= $linha['endereco'] ?>" class="<?= $linha['classe'] ?>"><?= $linha['descricao'] ?></a>
+    <?php
+    }
+    ?>
+</div>
+<hr>
--- trabalho/regiao.php
+++
@@ -1,8 +0,0 @@
-<form name="registrar" action= "registro.php" method="POST">
-    ID: <br>
-    <input type="number" name="id"><br>
-    Regiao: <br>
-    <input type="text" name="regiao"><br>
-    <input type="submit" name="enviar" value="enviar">
-
-</form>
