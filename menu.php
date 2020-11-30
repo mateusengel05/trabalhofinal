f8cf57c09660c1fd6be519e9743cdc5cce792005
Mateus <mateus.franca@unidavi.edu.br>
2020-11-30 13:44:07 -0300

    teste do menu

--- menu.php
+++ menu.php
@@ -18,3 +18,27 @@ include('conexao.php');
     <a class="flex-sm-fill text-sm-center nav-link" href="listagens/cadastrar/cidade.php">Cad. Cidades</a>
     <a class="flex-sm-fill text-sm-center nav-link" href="listagens/cadastrar/estados.php">Cad. Estados</a>
 </nav>
+
+<!-- MENU PROFESSOR -->
+
+<head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+</head>
+
+<?php
+include_once('conexao.php');
+$query = "SELECT * FROM menu ORDER BY ordem,descricao";
+$result = mysqli_query($conexao, $query);
+?>
+<div class="row">
+    <?php
+    foreach ($result as $linha) {
+    ?>
+        <a href="<?= $linha['endereco'] ?>" class="<?= $linha['classe'] ?>"><?= $linha['descricao'] ?></a>
+    <?php
+    }
+    ?>
+</div>
+<hr>
